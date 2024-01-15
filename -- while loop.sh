f28634517a77040f7fbb5c06026c64b7b3bb486c
MxJose <8370302+DChiefMX@users.noreply.github.com>
2024-01-15 11:35:05 -0500

    Update -- while loop.sh

--- -- while loop.sh
+++ -- while loop.sh
@@ -1,6 +1,7 @@
 -- while loop
 $ while true; do ( echo -n  'date +"%T - size:"' && curl -so /dev/null http://35.175.181.205/ -w '%{size_download}\n'; sleep 1 ; done
 
+
 1) connect to database using msql command
 2) $ show processlist; and see the last process wich is my connection
 
